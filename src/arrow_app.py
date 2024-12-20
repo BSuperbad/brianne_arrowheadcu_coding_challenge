c57e44aa4ea7547dd5712ecd7e0084165eee9fff
Brianne Wilhelmi <brianne@aibe.io>
2024-12-20 15:28:42 -0800

    #5. Sort Records: - Wrote helper function `remove_titles` to handle names with titles (e.g., "Mr.", "Mrs.") by temporarily removing them during sorting and preserving them when returning the sorted list. - Updated `sort_records` to use a `lambda` function as the sorting key, applying `remove_titles` to each record's name for proper sorting in ascending order.

--- src/arrow_app.py
+++ src/arrow_app.py
@@ -12,6 +12,7 @@ from db import user_database
 
 
 USERS_URL = "https://jsonplaceholder.typicode.com/users"
+TITLE_PATTERN = ["Mr.", "Mrs.", "Ms.", "Dr.", "Miss", "Ms."]
 
 
 # 1. FETCH USER DATA
@@ -20,6 +21,25 @@ def fetch_users(url):
     response = requests.get(url)
     response.raise_for_status()
     return response.json()
+
+
+# helper function to remove titles
+def remove_titles(name):
+    """Remove common titles from a name (TITLE_PATTERN) and then returns as is for sorting"""
+    for title in TITLE_PATTERN:
+        if name.startswith(title):
+            return name[len(title) :].strip()
+    return name
+
+
+# 5. SORT RECORDS BY NAME
+def sort_records(connection):
+    """Sort records by name, ignoring any title like 'Mr.' or 'Mrs.'"""
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM users ORDER BY name ASC;")
+    records = cursor.fetchall()
+    sorted_records = sorted(records, key=lambda r: remove_titles(r[1]))
+    return sorted_records
 
 
 def main():
@@ -41,6 +61,14 @@ def main():
         user_database.insert_user(connection, user)
         print("inserting users into db")
     connection.commit()
+
+    # 5. SORT RECORDS BY NAME ASCENDING
+    sorted_records = sort_records(connection)
+    print("#5. Sort Records:")
+    for r in sorted_records:
+        print(
+            f"ID: {r[0]}, Name: {r[1]}, Email: {r[3]}, Address: {r[6]}, {r[7]}, {r[8]}, {r[9]}"
+        )
 
 
 if __name__ == "__main__":
