87e48e770fdc3b42fb126c12c4267c7dbd77aec5
Brianne Wilhelmi <brianne@aibe.io>
2024-12-20 14:31:51 -0800

    #2. Display Records: Displaying all user records in the terminal by iterating through each one

--- src/arrow_app.py
+++ src/arrow_app.py
@@ -4,6 +4,7 @@ import requests
 
 USERS_URL = "https://jsonplaceholder.typicode.com/users"
 
+
 # 1. FETCH USER DATA
 def fetch_users(url):
     """Fetching all user records from json url"""
@@ -11,9 +12,15 @@ def fetch_users(url):
     response.raise_for_status()
     return response.json()
 
+
 def main():
     users = fetch_users(USERS_URL)
-    print(users)
+    print("#2. Display Records")
+    for user in users:
+        print(
+            f"ID: {user['id']}, Name: {user['name']}, Username: {user['username']}, Email: {user['email']}, Address: {user['address']['street']} {user['address']['suite']}, {user['address']['city']}, {user['address']['zipcode']}, Phone: {user['phone']}, Website: {user['website']}, Company: {user['company']['name']}, {user['company']['catchPhrase']} "
+        )
+
 
 if __name__ == "__main__":
     main()
